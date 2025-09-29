9acec20a19029ef0b69e45508ccf005bd831f4a7
Motaharul Islam <rimon.raton@gmail.com>
2025-09-29 15:08:19 +0600

    Updated DetailedAssessmentPage.tsx

--- src/pages/DetailedAssessmentPage.tsx
+++ src/pages/DetailedAssessmentPage.tsx
@@ -122,7 +122,7 @@ const DetailedAssessmentPage = () => {
       {/* Content */}
       <div className="flex-1 p-2">
         <div className="max-w-2xl mx-auto">
-          <div className="bg-white rounded-3xl p-8 shadow-lg">
+          <div className="bg-white rounded-3xl p-3 shadow-lg">
             {/* Header */}
             <div className="text-center mb-8">
               <h1 className="text-2xl font-bold text-gray-900 mb-3">
